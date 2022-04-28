c5612bc557dc9d95911fad77d3f47b7304f2affd
natasakis <natkis023@hotmail.com>
2022-04-28 19:05:33 +0200

    added hw11

---
+++ hw11/register.php
@@ -0,0 +1,35 @@
+<?php
+$name=$_GET['ime'];
+$name=ucfirst($name);
+$lastname=$_GET['prezime'];
+$lastname=ucfirst($lastname);
+$mail=$_GET['email'];
+$sifra=$_GET['password1'];
+$sifra2=$_GET['password2'];
+$gender=$_GET['pol'];
+$lista_kurseva=$_GET['kurs'];
+if (!isset($name)) {
+    echo "Niste upisali ime!";
+}else if (!isset($lastname)){
+    echo "Niste upisali prezime!";
+}else if (!isset($mail)){
+    echo "Niste upisali email!";
+}else if (!isset($sifra)){
+    echo "Niste uneli sifru!";
+}else if ($sifra!=$sifra2){
+    echo "Lozinke se ne poklapaju!";
+}else{
+    if ($gender=="Z"){
+        echo "Postovana ";
+    }
+    else if ($gender=="M") echo "Postovani ";
+    else echo "Postovana/i ";
+    echo "$name $lastname, <br>";
+    echo "Uspesno ste se registrovali na nasem sajtu.<br>";
+    echo "Vasa lozinka je: $sifra <br>";
+    echo "Vas username je: $mail <br>";
+    echo "Vasi odabrani kursevi su: ";
+    foreach ($lista_kurseva as $kurs)
+        echo $kurs ." ";
+}
+?>
