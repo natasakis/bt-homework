dcda011a858d218f96ae833303754c953620b75d
natasakis <natkis023@hotmail.com>
2022-04-28 13:38:05 +0200

    added second method

--- hw10.php
+++ hw10.php
@@ -16,6 +16,29 @@ echo "<br>";
 $division = $num1 / $num2;
 echo "Kolicnik je " .$division;
 echo "<br>";
+
+//zadatak 1-drugi nacin
+function sum($x,$y){
+    $sum2=$x+$y;
+    echo $sum2;
+}
+function product($x,$y){
+    $product2=$x*$y;
+    echo $product2;
+}
+function substract($x,$y){
+    $substract2=$x-$y;
+    echo $substract2;
+}
+function division($x,$y){
+    $division2=$x/$y;
+    echo $division2;
+}
+echo "Ako su brojevi $num1 i $num2 njihov zbir je: "; sum($num1,$num2);
+echo ", prizvod je: "; product($num1,$num2);
+echo ", razlika je: "; substract($num1,$num2);
+echo ", a kolicnik je: "; division($num1,$num2);
+echo ".<br>";
 echo "<br>";
 
 //zadatak2
@@ -44,14 +67,26 @@ switch ($r){
         echo "Danas je nedelja!";
         break;
     }
-    echo "<br>";
-    echo "<br>";
+echo "<br>";
+
+//zadatak2-drugi nacin
+$days=array("ponedeljak", "utorak", "sreda", "cetvrtak", "petak", "subota", "nedelja");
+echo "Danas je $days[$r]!";
+echo "<br>";
+echo "<br>";
 
 //zadatak3
 $a=rand(100,1000)/100;
 $b=rand(10,100)/10;
 $c=rand(1,10);
 echo "Zbir brojeva $a, $b i $c je " .$a+$b+$c .".";
+echo "<br>";
 
+//zadatak3-drugi nacin
+function add($x,$y,$z){
+    $w=$x+$y+$z;
+    echo $w;
+}
+echo "Zbir brojeva $a, $b i $c je "; add($a, $b, $c); echo "."; 
 
 ?>
